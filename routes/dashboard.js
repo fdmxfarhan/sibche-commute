e508eba54cbb817911124fcb1f6fd64e41c606b0
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-07-25 12:44:02 +0430

    general report

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -475,17 +475,6 @@ router.get('/report', ensureAuthenticated, (req, res, next) => {
             var usersTime = [];
             for (let i = 0; i < users.length; i++) {
                 sum = {hour: 0, minute: 0, second: 0}
-                // for(var j=0; j<commutes.length-1; j+=2){
-                //     if(commutes[j].personelID == users[i] && commutes[j+1].personelID == users[i]){
-                //         if(commutes[j].Enter && !commutes[j+1].Enter ) {
-                //             sum = sumTime(sum, deltaTime(commutes[j].time, commutes[j+1].time))
-                //         }
-                //         else if(!commutes[j].Enter)   
-                //             commutes.splice(j  , 0, 'undefined');
-                //         else if(commutes[j+1].Enter)  
-                //             commutes.splice(j+1, 0, 'undefined');
-                //     }
-                // }
                 usersTime.push(sum);
             }
             // console.log(usersTime);
@@ -771,5 +760,144 @@ router.post('/add-new-log', ensureAuthenticated, (req, res, next) => {
         }).catch(err => {if(err) console.log(err);});
     }
 });
+
+
+router.get('/admin-general-report', ensureAuthenticated, (req, res, next) => {
+    if(req.user.role == 'admin')
+    {
+        users = req.query.users.split(',');
+        var now = getNow();
+        var report = [];
+        for(var i=0; i<12; i++){
+            report.push({});
+            report[i].wb = new excel.Workbook({dateFormat: 'm/d/yy hh:mm:ss'});
+            report[i].style = report[i].wb.createStyle({
+                font: {
+                    color: '#111111',
+                    size: 12
+                },
+                numberFormat: '$#,##0.00; ($#,##0.00); -'
+            });
+            report[i].ws = report[i].wb.addWorksheet('گزارش کلی');
+            report[i].ws.cell(1,1).string(`شماره پرسنل`).style(report[i].style);
+            report[i].ws.cell(1,2).string(`net`).style(report[i].style);
+            report[i].ws.cell(1,3).string(`total`).style(report[i].style);
+            report[i].ws.cell(1,4).string(`تعداد حضور`).style(report[i].style);
+            report[i].ws.cell(1,5).string(`تعداد غیبت`).style(report[i].style);
+            report[i].ws.cell(1,6).string(`اضافه کاری`).style(report[i].style);
+            report[i].ws.cell(1,7).string(`تاخیر`).style(report[i].style);
+            report[i].ws.cell(1,8).string(`غیبت ساعتی`).style(report[i].style);
+        }
+        Commute.find({}, (err, allCommutes) => {
+            data = [];
+            for(var userNum = 0; userNum < users.length; userNum++){
+                var commutes = allCommutes.filter(element => element.personelID == users[userNum]);
+                commutes.sort(sortAlgorithm);
+                for(var monthNum=1; monthNum<=12; monthNum++){
+                    monthData = [];
+                    cnt = 2;
+                    for(var i=0; i<dateConvert.j_days_in_month[monthNum-1]; i++){
+                        var j_year = now.year;
+                        var j_month = monthNum;
+                        var j_day = i+1;
+                        var j_date = dateConvert.jalali_to_gregorian(j_year, j_month, j_day);
+                        var j_d = new Date(j_date[0], j_date[1]-1, j_date[2], 12, 0, 0, 0);
+                        var thisCommutes = [];
+                        for (let j = 0; j < commutes.length; j++) {
+                            if(commutes[j].j_date.year == j_year && commutes[j].j_date.month == j_month && commutes[j].j_date.day == j_day)
+                                thisCommutes.push(commutes[j]);
+                        }
+                        monthData.push({year: j_year, month: j_month, day: j_day, date: j_d, commutes: thisCommutes});
+                    }
+                    monthSum = {hour: 0, minute: 0, second: 0};
+                    monthTotal = {hour: 0, minute: 0, second: 0};
+                    monthExtra = {hour: 0, minute: 0, second: 0};
+                    monthEnterLate = {hour: 0, minute: 0, second: 0};
+                    monthLate = {hour: 0, minute: 0, second: 0};
+                    presence = 0;
+                    apsence = 0;
+                    colorCnt = 0;
+                    for(var i=0; i<monthData.length; i++){
+                        var correct = 0, incorrect = 0, sum = {hour: 0, minute: 0, second: 0}, total = {hour: 0, minute: 0, second: 0};
+                        for(var j=0; j<monthData[i].commutes.length-1; j+=2){
+                            if(monthData[i].commutes[j].Enter && !monthData[i].commutes[j+1].Enter ) {
+                                correct++; 
+                                sum = sumTime(sum, deltaTime(monthData[i].commutes[j].time, monthData[i].commutes[j+1].time))
+                                delta = deltaTime(monthData[i].commutes[j].time, monthData[i].commutes[j+1].time);
+                                cnt++;
+                            }
+                            else if(!monthData[i].commutes[j].Enter)    {incorrect++;monthData[i].commutes.splice(j  , 0, 'undefined');}
+                            else if(monthData[i].commutes[j+1].Enter)   {incorrect++;monthData[i].commutes.splice(j+1, 0, 'undefined');}
+                            else incorrect++;
+                        }
+                        var start = {hour: 0, minute: 0, second: 0}, end = {hour: 0, minute: 0, second: 0};
+                        for(var j=0; j<monthData[i].commutes.length; j++)
+                            if(monthData[i].commutes[j] != 'undefined' && monthData[i].commutes[j].Enter) {start = monthData[i].commutes[j].time; break;}
+                        for(var j=monthData[i].commutes.length-1; j>0; j--)
+                            if(monthData[i].commutes[j] != 'undefined' && !monthData[i].commutes[j].Enter) {end = monthData[i].commutes[j].time; break;}
+                        var total = deltaTime(start, end);
+
+                        if(monthData[i].commutes.length != 0){
+                            colorCnt++;
+                        }
+                        if(getSec(start) < 8.5 * 60 * 60 && getSec(start) > 0) start = {hour: 8, minute: 30, second: 0};
+                        total = deltaTime(start, end);
+                        if(dateConvert.day_in_week(monthData[i].date) == 'پنج شنبه' || dateConvert.day_in_week(monthData[i].date) == 'جمعه'){
+                            // fullTime.cell(i+2,5).string(`تعطیل`).style(styleGray);
+                            if(getSec(sum) > 0){
+                                // fullTime.cell(i+2,6).string(timeToString(sum)).style(styleGreen);
+                            }
+                        }else if(getSec(sum) < 7.5 * 60 * 60){
+                            // fullTime.cell(i+2,5).string(`غیبت (ساعتی)`).style(styleRed);
+                            late = deltaTime(sum, {hour: 7, minute: 30, second: 0});
+                            monthLate = sumTime(monthLate, late);
+                            apsence++;
+                            // fullTime.cell(i+2,8).string(timeToString(late)).style(styleRed);
+                        }else if(getSec(start) > 10 * 60 * 60){
+                            // fullTime.cell(i+2,5).string(`غیبت (تاخیر در ورود)`).style(styleRed);
+                            late = deltaTime(start, {hour: 10, minute: 0, second: 0});
+                            monthEnterLate = sumTime(monthEnterLate, late);
+                            apsence++;
+                            // fullTime.cell(i+2,7).string(timeToString(late)).style(styleRed);
+                        }else{
+                            // fullTime.cell(i+2,5).string(`حضور`).style(styleGreen);
+                            presence++;
+                            if(getSec(total) > 9 * 60 * 60){
+                                extra = deltaTime(sum, {hour: 7, minute: 30, second: 0});
+                                monthExtra = sumTime(monthExtra, extra);
+                                // fullTime.cell(i+2,6).string(timeToString(extra)).style(styleGreen);
+                            }
+                        }
+                        monthSum = sumTime(monthSum, sum);
+                        monthTotal = sumTime(monthTotal, total);
+                    }
+                    report[monthNum-1].ws.cell(userNum+2,1).string(`${users[userNum]}`).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,2).string(timeToString(monthSum)).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,3).string(timeToString(monthTotal)).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,4).string(`${presence}`).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,5).string(`${apsence}`).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,6).string(timeToString(monthExtra)).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,7).string(timeToString(monthEnterLate)).style(report[monthNum-1].style);
+                    report[monthNum-1].ws.cell(userNum+2,8).string(timeToString(monthLate)).style(report[monthNum-1].style);
+                }
+            }
+            if(!fs.existsSync(path.join(__dirname, "/../public/output")))
+            fs.mkdirSync(path.join(__dirname, "/../public/output"));
+            for(var i=0; i<12; i++){
+                report[i].wb.write(`./public/output/گزارش_کلی_${dateConvert.get_persian_month(i+1)}.xlsx`);
+                data.push({});
+                data[i].out = `/output/گزارش_کلی_${dateConvert.get_persian_month(i+1)}.xlsx`;
+                data[i].name = `${dateConvert.get_persian_month(i+1)}`;
+            }
+            res.render('./dashboard/admin-general-report', {
+                user: req.user,
+                report: data,
+            });
+        });
+    }
+    
+});
+
+
 
 module.exports = router;
